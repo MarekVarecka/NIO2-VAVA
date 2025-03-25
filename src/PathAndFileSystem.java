a5cff6599d7646dc2a77dac9eab92e828ebd07f0
MarekVarecka <marekvarecka@gmail.com>
2025-03-25 16:29:11 +0100

    Update PathAndFileSystem.java

--- src/PathAndFileSystem.java
+++ src/PathAndFileSystem.java
@@ -1,27 +1,20 @@
-import javax.xml.xpath.XPath;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.List;
 
 public class PathAndFileSystem {
-    String content =
-            "Sigma-sigma boy, sigma boy, sigma boy\n" +
-            "Каждая девчонка хочет танцевать с тобой\n" +
-            "Sigma-sigma boy, sigma boy, sigma boy\n" +
-            "Я такая вся, что добиваться будешь год\n" +
-            "Sigma-sigma boy, sigma boy, sigma boy\n" +
-            "Каждая девчонка хочет танцевать с тобой\n" +
-            "Sigma-sigma boy, sigma boy, sigma boy\n" +
-            "Я такая вся, что добиваться будешь год\n" +
-            "Sigma-sigma boy, sigma boy, sigma boy\n" +
-            "Каждая девчонка хочет танцевать с тобой\n" +
-            "Sigma-sigma boy, sigma boy, sigma boy\n" +
-            "Я такая вся, что добиваться будешь год";
-
     public static void createFile(String filePath) throws IOException {
+
         Path path = Paths.get(filePath);
-        Files.createFile(path);
+        if (!Files.exists(path)) {
+            Files.createFile(path);
+        }
+    }
+
+    public static void writeToFile(String filePath, String content) throws IOException {
+        Path path = Paths.get(filePath);
+        Files.write(path, content.getBytes(StandardCharsets.UTF_8));
     }
 
     public static void readFromFile(String filePath) throws IOException {
@@ -38,6 +31,12 @@ public class PathAndFileSystem {
     public static void copyFile(String sourcePath, String destinationPath) throws IOException {
         Path from = Paths.get(sourcePath);
         Path to = Paths.get(destinationPath);
+//        Java ENUM
+//        StandardCopyOption.REPLACE_EXISTING
+//          = If the target file already exists, it will be replaced
+//        StandardCopyOption.COPY_ATTRIBUTES
+//          =
+//        StandardCopyOption.ATOMIC_MOVE
             Files.copy(from, to, StandardCopyOption.REPLACE_EXISTING);
             System.out.println("File copied from " + sourcePath + " to " + destinationPath);
     }
@@ -50,9 +49,6 @@ public class PathAndFileSystem {
     public static void moveFile(String sourcePath, String destinationPath) throws IOException {
         Path from = Paths.get(sourcePath);
         Path to = Paths.get(destinationPath);
-//        StandardCopyOption.REPLACE_EXISTING
-//        StandardCopyOption.COPY_ATTRIBUTES
-//        StandardCopyOption.ATOMIC_MOVE
         Files.move(from, to, StandardCopyOption.REPLACE_EXISTING);
         System.out.println("File moved from " + sourcePath + " to " + destinationPath);
     }
@@ -62,12 +58,19 @@ public class PathAndFileSystem {
         String copyPath = "./copy.txt";
         String dirPath = "./newdir";
         String moveTarget = "./newdir/text.txt";
-
+        String content = "This is content of the text file";
+        String copyTarget = "./copy.txt";
         createFile(filePath);
+
+        writeToFile(filePath, content);
+
         readFromFile(filePath);
+
         copyFile(filePath, copyPath);
+
         createDirectory(dirPath);
-        moveFile(filePath, moveTarget);
+        
+        moveFile(copyTarget, moveTarget);
     }
 }
 
